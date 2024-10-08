2fcebef2b844edd4a7af9f83d3c36b76b86d6e10
Nicolas Rio <nicolas.rio42@gmail.com>
2024-10-08 18:40:59 +0200

    WIP

---
+++ uno.config.ts
@@ -0,0 +1,10 @@
+import {
+  defineConfig,
+  presetUno,
+  presetAttributify,
+  presetTypography,
+} from "unocss";
+
+export default defineConfig({
+  presets: [presetUno(), presetAttributify(), presetTypography()],
+});
